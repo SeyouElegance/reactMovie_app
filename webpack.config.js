811ca75560b54b82fdbb45ae2484920a579f800f
Semmy HAKIMI <hakimisemmy@gmail.com>
2018-06-21 10:13:11 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? "warning" : false
+  },
+  module:{
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:{
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|TTF)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
+      {
+        test: /\.exec\.js$/,
+        use: [ 'script-loader' ]
+      },
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
