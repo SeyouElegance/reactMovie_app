a7b264b7770a9574424762353bdca9c39c123047
Semmy H <shakimi@laprovence.com>
2022-03-14 17:17:11 +0100

    maj

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(png|jpg|gif|TTF)$/,
+        test: /\.(png|jpg|gif|ttf)$/,
         use: [
           {
             loader: 'file-loader',
